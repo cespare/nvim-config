8911d186fa54af762fb0af4a9acc53801b8a3ff1
Caleb Spare <cespare@gmail.com>
2022-10-08 15:21:56 -0700

    Remove pretty_print now that I'm using neovim 0.7.0+

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,18 +1,6 @@
 local stringx = require("stringx")
 local a = vim.api
 
--- TODO: Copied from nvim source code. Delete after I switch to 0.7.0+.
-function pretty_print(...)
-  local objects = {}
-  for i = 1, select('#', ...) do
-    local v = select(i, ...)
-    table.insert(objects, vim.inspect(v))
-  end
-
-  print(table.concat(objects, '    '))
-  return ...
-end
-
 function echom(...)
   vim.api.nvim_echo({{table.concat({...}, " ")}}, true, {})
 end
