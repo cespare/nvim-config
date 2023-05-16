6c5f44dd70d786a74d5682e8566887233172f5f9
Caleb Spare <cespare@gmail.com>
2023-05-16 13:30:17 -0700

    Improve LSP floating windows a bit

--- lua/init.lua
+++ lua/init.lua
@@ -3,6 +3,19 @@ local lspconfig = require('lspconfig')
 -- I don't want diagnostics to show up at all except via the quickfix list.
 vim.diagnostic.config({underline = false, virtual_text = false, signs = false})
 
+-- Use some monkey-patching to configure all floating windows (mostly used for
+-- LSP features like hover). It's kind of hacky, but apparently this is how it's
+-- done.
+local prev_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or 'rounded'
+  opts.max_width = opts.max_width or 80
+  local bufnr, winnr = prev_util_open_floating_preview(contents, syntax, opts, ...)
+  vim.api.nvim_win_set_option(winnr, "linebreak", true)
+  return bufnr, winnr
+end
+
 -- Set up LSP key bindings.
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
@@ -21,6 +34,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     )
 
     vim.keymap.set('n', '<leader>gh', vim.lsp.buf.document_highlight, opts)
+    vim.keymap.set('i', '<C-i>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<leader>gi', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<leader>gf', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>gr', vim.lsp.buf.rename, opts)
